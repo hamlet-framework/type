5ee0e9adf0683167e4cdf13fc1f9ff79d7fb3187
Vasily Kartashov <info@kartashov.com>
2020-06-17 22:04:12 +1000

    psalm fixes

--- src/Parser/Cache.php
+++ src/Parser/Cache.php
@@ -8,16 +8,18 @@ class Cache
 {
     /**
      * @param string $key
-     * @param Type $value
+     * @param Type $type
      * @return void
+     * @psalm-suppress MixedAssignment
+     * @psalm-suppress MixedOperand
      */
-    public static function set(string $key, Type $value)
+    public static function set(string $key, Type $type)
     {
         if (extension_loaded('apcu')) {
-            apcu_store($key, [$value, time()]);
+            apcu_store($key, [$type, time()]);
         } else {
             $fileName = sys_get_temp_dir() . '/' . md5($key);
-            $payload = $value->serialize();
+            $payload = $type->serialize();
             $tempFileName = $fileName . '.tmp';
             file_put_contents($tempFileName, '<?php $value = ' . $payload . ';');
             rename($tempFileName, $fileName);
@@ -28,6 +30,10 @@ class Cache
      * @param string $key
      * @param int $timeThreshold
      * @return Type|null
+     * @psalm-suppress MixedArrayAccess
+     * @psalm-suppress MixedAssignment
+     * @psalm-suppress MixedInferredReturnType
+     * @psalm-suppress MixedReturnStatement
      */
     public static function get(string $key, int $timeThreshold)
     {
